ed32f1236c4a994130d6da1c0beed6a5628d87a1
TanzirKhan <47850858+TanzirKhan@users.noreply.github.com>
2019-11-21 15:09:04 +0000

    Delete Planes.js

--- Traffic control application/Planes.js
+++
@@ -1,7 +0,0 @@
-class Planes{
-    constructor(model, landingTime, takeOffTime){
-        this.model = model;
-        this.landingTime = landingTime;
-        this.takeOffTime = takeOffTime;
-    }
-}
